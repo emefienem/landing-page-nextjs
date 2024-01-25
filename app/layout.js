4af0c4c5ee5eaeffb06f43272250800590619eff
emefienem <emefienemmichael@gmail.com>
2024-01-24 17:21:24 -0800

    bug

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-[#1F1548]">{children}</body>
+      <body className="bg-[#1F1548] overflow-hidden">{children}</body>
     </html>
   );
 }
